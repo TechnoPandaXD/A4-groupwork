a4c4a572a9313fb246e6870c03e9e75f6ab3a190
TechnoPandaXD <kle256@mtroyal.ca>
2026-04-07 21:01:33 -0600

    Starter files

---
+++ src/IntBoxNode.java
@@ -0,0 +1,52 @@
+import java.awt.Paint;
+import java.awt.Shape;
+
+import org.piccolo2d.nodes.PPath;
+import org.piccolo2d.nodes.PText;
+import org.piccolo2d.util.PBounds;
+
+/**
+ * A node that is a combination of a shape and an integer within it.
+ */
+public class IntBoxNode extends PPath.Float {
+    private static final long serialVersionUID = 1L;
+
+    private PText nodeLabel;
+
+    private int number;
+
+    public IntBoxNode(Shape shape, int newNumber) {
+        super(shape);
+
+        this.nodeLabel = new PText(Integer.toString(newNumber));
+        this.nodeLabel.setPickable(false);
+        this.addChild(nodeLabel);
+        this.centerText();
+
+        this.number = newNumber;
+    }
+
+    public Paint getTextPaint() {
+        return this.nodeLabel.getTextPaint();
+    }
+
+    public void setTextPaint(Paint textPaint) {
+        this.nodeLabel.setTextPaint(textPaint);
+    }
+
+    public int getNumber() {
+        return this.number;
+    }
+
+    public void setText(int newNumber) {
+        this.nodeLabel.setText(Integer.toString(newNumber));
+        this.centerText();
+        this.number = newNumber;
+    }
+
+    private void centerText() {
+        PBounds ourBounds = this.getBounds();
+        this.nodeLabel.centerBoundsOnPoint(ourBounds.getCenterX(), ourBounds.getCenterY());
+    }
+
+}
---
+++ src/SortingAnimation.java
@@ -0,0 +1,116 @@
+import java.awt.Color;
+import java.awt.Font;
+
+import org.piccolo2d.activities.PActivity;
+import org.piccolo2d.nodes.PText;
+
+public class SortingAnimation extends AnimationScreen {
+    private static final long serialVersionUID = 1L;
+
+    private static final int totalWidth = 500;
+    private static final int totalHeight = 300;
+
+    private PText header;
+    private IntBoxNode[] myNumberBoxes;
+
+    @Override
+    public void addInitialNodes() {
+        setBounds(0, 0, totalWidth, totalHeight);
+
+        // add background as a coloured box
+        addColouredBox(0, 0, totalWidth, totalHeight, Color.LIGHT_GRAY);
+
+        // add header text
+        header = addText(0, 0, "Sorting is cool!");
+        header.setTextPaint(Color.DARK_GRAY);
+
+        // You could use an array of int nodes to hold numbers
+        // to visualize the array.
+        myNumberBoxes = new IntBoxNode[2];
+
+        int num1 = (int) (Math.random() * 40 + 1);
+        myNumberBoxes[0] = addIntBox(-30, -30, 30, 30, num1);
+        myNumberBoxes[0].setPaint(Color.BLUE);
+        myNumberBoxes[0].setTextPaint(Color.RED);
+
+        int num2 = (int) (Math.random() * 40 + 1);
+        myNumberBoxes[1] = addIntBox(-50, -50, 30, 30, num2);
+        myNumberBoxes[1].setPaint(Color.BLUE);
+        myNumberBoxes[1].setTextPaint(Color.RED);
+    }
+
+    public void playAnimation() {
+        // wait for initialization before animating
+        waitForInitialization();
+
+        // Some example animation steps:
+
+        // Parameters are x, y, scale, rotation (in radians), and time (in ms)
+        waitForActivity(myNumberBoxes[0].animateToPositionScaleRotation(10, 30, 0.5, 0, 1000));
+
+        // The function waitForActivity makes sure the following animation doesn't start
+        // until after this animation step is done
+        waitForActivity(myNumberBoxes[0].animateToColor(Color.GREEN, 2000),
+                myNumberBoxes[0].animateToPositionScaleRotation(100, 30, 1.5, Math.toRadians(110), 2000));
+
+        // Multiple items can be animated at the same time. You just need to give them
+        // the same duration if you want them to move at the same speed. Lower duration
+        // steps would finish first, like the header move here.
+        waitForActivity(header.animateToPositionScaleRotation(0, 0, 2.0, 0, 500),
+                myNumberBoxes[0].animateToPositionScaleRotation(150, 110, 1, 0, 1000),
+                myNumberBoxes[1].animateToColor(Color.GREEN, 1000),
+                myNumberBoxes[1].animateToPositionScaleRotation(200, 110, 1, 0, 1000));
+
+        // You can show comparing with colour change
+        waitForActivity(myNumberBoxes[0].animateToColor(Color.PINK, 1000),
+                myNumberBoxes[1].animateToColor(Color.PINK, 1000));
+
+        // If you don't wait, then these will happen at the same time as the following
+        // steps
+        myNumberBoxes[0].animateToColor(Color.GREEN, 1000);
+        myNumberBoxes[1].animateToColor(Color.GREEN, 1000);
+
+        // You could use the number in boxes for comparisons
+        if (myNumberBoxes[0].getNumber() > myNumberBoxes[1].getNumber()) {
+            // You can even swap values in the array
+            IntBoxNode temp = myNumberBoxes[0];
+            myNumberBoxes[0] = myNumberBoxes[1];
+            myNumberBoxes[1] = temp;
+
+            // But make sure you animate it too, or it won't be visible.
+            // Hint: the positions should really be computed from the index.
+            waitForActivity(myNumberBoxes[0].animateToPositionScaleRotation(150, 110, 1, 0, 2000),
+                    myNumberBoxes[1].animateToPositionScaleRotation(200, 110, 1, 0, 2000));
+        }
+
+        // You can add items as you go...
+        PText allDone = addText(100, 200, "All Done!");
+        allDone.setFont(new Font("Consolas", Font.PLAIN, 36));
+        allDone.setTextPaint(Color.WHITE);
+        // but sometimes they don't show up unless you animate them.
+        // You can use a delay of zero, though.
+        allDone.animateToPositionScaleRotation(100, 200, 1, 0, 0);
+    }
+
+    public static void main(String[] args) {
+        SortingAnimation screen = new SortingAnimation();
+        screen.playAnimation();
+    }
+
+    private static void waitForActivity(PActivity... activities) {
+        long finalEndTime = 0;
+        for (PActivity activity : activities) {
+            long thisEndTime = activity.getStartTime() + activity.getDuration();
+            if (thisEndTime > finalEndTime) {
+                finalEndTime = thisEndTime;
+            }
+        }
+        while (System.currentTimeMillis() < finalEndTime) {
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                // Whatever, I do what I want.
+            }
+        }
+    }
+}
