1725858438734e1771e7e5231c085bc96f1add9c
Louise Pablo <lpabl058@mroyal.ca>
2026-04-09 21:49:30 -0600

    Added comments for animation

--- src/AnimationScreen.java
+++ src/AnimationScreen.java
@@ -8,43 +8,56 @@ import org.piccolo2d.nodes.PText;
 
 /**
  * A base simplified animation screen. This class should be used as a
- * super-class for a sorting animation class.
+ * super-class for a sorting animation class. -- provides helper methods
+ * for creating and displaying visual elements
  */
 public abstract class AnimationScreen extends PFrame {
     private static final long serialVersionUID = 1L;
 
+    // Tracks whether the screen has finished initializing
     private boolean isInitialized;
 
     public AnimationScreen() {
         super();
 
+        // Initially, the screen is not ready
         isInitialized = false;
     }
 
     public final void initialize() {
-        // Remove the default pan and zoom event handlers
+        // Remove the default pan and zoom event handlers (mouse controls)
         getCanvas().setPanEventHandler(null);
         getCanvas().setZoomEventHandler(null);
 
+        // Let the subclass add its initial visual elements
         addInitialNodes();
 
+        // Mark the screen as ready for animation
         isInitialized = true;
     }
 
     public abstract void addInitialNodes();
 
+    /**
+     * This method must be implemented by subclasses
+     * to define what appears on the screen at the start.
+     */
     public void waitForInitialization() {
+        // Wait until initialization is complete before continuing
         while (!isInitialized) {
             try {
                 Thread.sleep(100);
             } catch (InterruptedException e) {
-                // Whatever, I do what I want.
+                // Ignore interruption
             }
         }
     }
 
+      /**
+     * Positions a node and adds it to the animation screen.
+     */
     private void addAt(PNode node, double x, double y) {
-        // position item at location
+        // position item(node) at specific location
         node.translate(x, y);
 
         // add to canvas (screen)
@@ -52,6 +65,9 @@ public abstract class AnimationScreen extends PFrame {
         getCanvas().repaint();
     }
 
+    /**
+     * Creates and displays a text box.
+     */
     public TextBoxNode addTextBox(double x, double y, double width, double height, String text) {
         TextBoxNode newNode = new TextBoxNode(new Rectangle2D.Double(0, 0, width, height), text);
 
@@ -60,6 +76,9 @@ public abstract class AnimationScreen extends PFrame {
         return newNode;
     }
 
+     /**
+     * Creates and displays a box containing an integer.
+     */
     public IntBoxNode addIntBox(double x, double y, double width, double height, int number) {
         IntBoxNode newNode = new IntBoxNode(new Rectangle2D.Double(0, 0, width, height), number);
 
@@ -68,6 +87,9 @@ public abstract class AnimationScreen extends PFrame {
         return newNode;
     }
 
+    /**
+     * Creates and displays simple text.
+     */
     public PText addText(double x, double y, String text) {
         PText newNode = new PText(text);
 
@@ -76,10 +98,15 @@ public abstract class AnimationScreen extends PFrame {
         return newNode;
     }
 
+    /**
+     * Creates and displays a colored rectangle (background or box).
+     */
     public PNode addColouredBox(double x, double y, double width, double height, Paint colour) {
         PNode newNode = new PNode();
-        // A node will not be visible until its bounds and paint are set.
+        // note - node will not be visible until its bounds and paint are set.
+        // Set size of the box
         newNode.setBounds(0, 0, width, height);
+        // set colour
         newNode.setPaint(colour);
 
         addAt(newNode, x, y);
@@ -87,6 +114,9 @@ public abstract class AnimationScreen extends PFrame {
         return newNode;
     }
 
+     /**
+     * Creates and displays an ellipse shape.
+     */
     public PPath addElipse(double x, double y, double width, double height) {
         PPath newNode = PPath.createEllipse(0, 0, width, height);
 
@@ -94,7 +124,10 @@ public abstract class AnimationScreen extends PFrame {
 
         return newNode;
     }
-
+    
+     /**
+     * Creates and displays a rectangle shape.
+     */
     public PPath addRectangle(double x, double y, double width, double height) {
         PPath newNode = PPath.createRectangle(0, 0, width, height);
 
@@ -102,9 +135,12 @@ public abstract class AnimationScreen extends PFrame {
 
         return newNode;
     }
-
-    // Caution: lines are difficult to animate, so they are only recommend
-    // if they don't move.
+/**
+    * Caution: lines are difficult to animate, so they are only recommend
+    * if they don't move.
+     * Creates and displays a line.
+     * Note: Lines are harder to animate, so they are best used as static elements.
+     */
     public PPath addLine(double x1, double y1, double x2, double y2) {
         PPath newNode = PPath.createLine(x1, y1, x2, y2);
 
--- src/SortingAnimation.java
+++ src/SortingAnimation.java
@@ -7,91 +7,119 @@ import org.piccolo2d.nodes.PText;
 public class SortingAnimation extends AnimationScreen {
     private static final long serialVersionUID = 1L;
 
+    // Size of the whole window
     private static final int totalWidth = 600;
     private static final int totalHeight = 300;
+    
+    // Size of each box (the numbers)
     private static final int BOX_SIZE = 30;
+    
+    // Space between each box
     private static final int SPACING = 10;
+    
+    // Space from the left side
     private static final int MARGIN = 50;
+
+    // Height used when moving boxes (so they "lift up")
     private static final int ROW_Y = 150;
     
+    // Text at the top (used to show status)
     private PText header;
+
+    // Array that holds all the number boxes
     private IntBoxNode[] myNumberBoxes;
 
-    private static final int ARRAY_SIZE = 12; // Assignment requires 12 elements
+    private static final int ARRAY_SIZE = 12; // Assignment requires 12 elements (12 numbers)
+
     @Override
     public void addInitialNodes() {
+        // Set screen size
         setBounds(0, 0, totalWidth, totalHeight);
 
         // add background as a coloured box
         addColouredBox(0, 0, totalWidth, totalHeight, Color.LIGHT_GRAY);
 
-        // TODO: change header text to actual algorithm name
+        // TODO: change header text to actual algorithm name (title text at the top)
         header = addText(0, 0, "Sorting is cool!");
         header.setTextPaint(Color.DARK_GRAY);
 
-        // You could use an array of int nodes to hold numbers
-        // to visualize the array.
+        // Create the array for the boxes
         myNumberBoxes = new IntBoxNode[ARRAY_SIZE];
+        // Fill it with random numbers
         createRandomBoxes();
     }
 
     public void playAnimation() {
-        // wait for initialization before animating
+        // wait for initialization before animating (before start)
         waitForInitialization();
-
+        
+        // Start the sorting algorithm
         ShellSort.sort(myNumberBoxes, this);
         PText allDone = addText(100, 200, "All Done!");
         allDone.setFont(new Font("Consolas", Font.PLAIN, 36));
         allDone.setTextPaint(Color.WHITE);
+
+
         // but sometimes they don't show up unless you animate them.
-        // You can use a delay of zero, though.
+        // You can use a delay of zero, though to make sure they actually show up
         allDone.animateToPositionScaleRotation(100, 200, 1, 0, 0);
     }
 
     public static void main(String[] args) {
+        // start animation program
         SortingAnimation screen = new SortingAnimation();
         screen.playAnimation();
     }
-
+    // Waits for animations to finish before continuing
     private static void waitForActivity(PActivity... activities) {
         long finalEndTime = 0;
+
+        // Find the latest ending animation
         for (PActivity activity : activities) {
             long thisEndTime = activity.getStartTime() + activity.getDuration();
             if (thisEndTime > finalEndTime) {
                 finalEndTime = thisEndTime;
             }
         }
+
+        // Pause until animations are done
         while (System.currentTimeMillis() < finalEndTime) {
             try {
                 Thread.sleep(100);
             } catch (InterruptedException e) {
-                // Whatever, I do what I want.
+                // ignore
             }
         }
     }
     
-    /*
-     * NEEDS TO BE IMPLEMENTED (REMOVE TODO comments after we do it)
-     */
     
     // loop through array and fill indexes with random numbers.
+    // will create random boxes with numbers
     private void createRandomBoxes() {
         for (int i = 0; i < ARRAY_SIZE; i++) {
+            // random number from 1 - 40
             int num = (int) (Math.random() * 40 + 1);
+
+            //create box at the correct position
             myNumberBoxes[i] = addIntBox(getBoxX(i), getBoxY(), BOX_SIZE, BOX_SIZE, num);
+            
+            // set colours
             myNumberBoxes[i].setPaint(Color.BLUE);
             myNumberBoxes[i].setTextPaint(Color.RED);
         }
     }
 
+    // Calculates X position based on index
     private double getBoxX(int index) {
        return MARGIN + index * (BOX_SIZE + SPACING);
     }
 
+    // Gets Y position (middle of screen)
     private double getBoxY() {
         return totalHeight / 2;
     }
 
+    // Highlights two boxes when comparing
     public void highlightCompare(int i, int j) {
         waitForActivity(
             myNumberBoxes[i].animateToColor(Color.PINK, 400),
@@ -99,6 +127,7 @@ public class SortingAnimation extends AnimationScreen {
         );
     }
 
+    // Resets color after comparing
     public void resetCompare(int i, int j) {
         waitForActivity(
             myNumberBoxes[i].animateToColor(Color.BLUE, 300),
@@ -107,10 +136,12 @@ public class SortingAnimation extends AnimationScreen {
     }
 
 
+    // Moves a value from one index to another (shift)
     public void shiftBox(int from, int to) {
-        // IMPORTANT: value is already copied into "to"
+        // get the value being moved
         int value = myNumberBoxes[to].getNumber();
 
+        // Create a temporary moving box
         IntBoxNode temp = addIntBox(getBoxX(from), getBoxY(), BOX_SIZE, BOX_SIZE, value);
         temp.setPaint(Color.GREEN);
         temp.setTextPaint(Color.RED);
@@ -130,15 +161,17 @@ public class SortingAnimation extends AnimationScreen {
             temp.animateToPositionScaleRotation(getBoxX(to), getBoxY(), 1, 0, totalHeight)
         );
 
+        // remove temp box after move
         temp.removeFromParent();
     }
-
+    // Moves a box directly to a new index
     public void moveBoxToIndex(IntBoxNode box, int index) {
         waitForActivity(
             box.animateToPositionScaleRotation(getBoxX(index), getBoxY(), 1, 0, totalWidth)
         );
     }
 
+    // Updates the text at the top
     public void updateStatus(String text) {
         header.setText(text);
     }
